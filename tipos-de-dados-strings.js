72900043813b0db854f06c31b98be693c4342e3d
エンツォ・ジオヴァーネ・ザビエル・サントス <en964776@gmail.com>
2026-09-11 09:12:38 -0300

    tipos-de-dados-strings.js

---
+++ tipos-de-dados-strings.js
@@ -0,0 +1,7 @@
+let nome = "Ana"; // String com aspas duplas
+let sobrenome = 'Silva'; // String com aspas simples
+let saudacao = `Olá, ${nome}!`; // Template literal com interpolação de variáveis
+
+console .log("nome:", nome)
+console .log("sobrenome:", sobrenome)
+console .log("saudação:", saudacao)
