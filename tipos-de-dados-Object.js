0b421d95a04d385b8b880688a38fa6a8690ac57d
エンツォ・ジオヴァーネ・ザビエル・サントス <en964776@gmail.com>
2026-09-11 10:53:36 -0300

    tipos-de-dados-Object.js

---
+++ tipos-de-dados-Object.js
@@ -0,0 +1,9 @@
+let pessoa = {
+    nome: "Cralos",
+    idade: 28,
+    casado: true,
+};
+// Acessando propriedades do objeto
+console.log(pessoa.nome); //"Carlos"
+console.log(pessoa.idade); //28
+console.log(pessoa.casado) // casado
