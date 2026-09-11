f980e3a262433e20febd35a911368b70c6181e40
エンツォ・ジオヴァーネ・ザビエル・サントス <en964776@gmail.com>
2026-09-11 10:37:28 -0300

    tipos-de-dados-Array.js

---
+++ tipos-de-dados-Array.js
@@ -0,0 +1,7 @@
+let frutas = ["Maçã", "Banana", "laranja"]; // Array de strings
+let numeros = [1, 2, 3, 4, 5,]; //Array de Números
+let misto = [true, "texto", 42]; // Array com diferentes tipos de dados
+
+console .log("frutas")
+console .log("numeros")
+console .log("misto")
