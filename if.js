67ed12d1612a7aac67037de16b59783d1572c578
エンツォ・ジオヴァーネ・ザビエル・サントス <en964776@gmail.com>
2026-09-22 08:32:19 -0300

    if.js

---
+++ if.js
@@ -0,0 +1,7 @@
+let hora = 15;
+if  (hora = 12) {
+    console.log("bom dia!") ;
+}
+
+// o restante do codigo continua independente se a codição for verdadeiro ou não
+console.log("dinheiro não traz felicidade. Mq dá o seu e seja feliz")
